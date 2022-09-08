f09215bc6d819f8a4fb7d1109949656592d65d47
HAPPY231 <marszycki056@gmail.com>
2022-09-08 05:22:37 +0200

    HomeControlerChanges

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -20,33 +20,34 @@ class HomeController extends Controller
     public function all_products(Request $request): View|JsonResponse
     {
         $filter = $request->query('filter');
-        $paginate = $request->query('paginate');
         $products = DB::table('products')->select('products.*');
 
+        function get_paginate($request){
+            $paginate = $request->query('paginate');
+            if(!$paginate) return 10;
+            if($paginate==1010) return 10;
+            if($paginate==1515) return 15;
+            if($paginate==2020) return 20;
+            if($paginate==100100) return 100;
 
-        if(!$paginate){
-            $paginate=10;
-        }else{
-            $paginate = (int)$paginate;
+            return (int)$paginate;
         }
-        $products->paginate($paginate);
+        $products->paginate(get_paginate($request));
         if(!is_null($filter)){
             if(array_key_exists('categories',$filter)) {
                 $products = $products->whereIn('category_id', $filter['categories']);
-                $amount = 0;
-                foreach($filter['categories'] as $categirue){
-                    $categories_children = Categories::where('parent_id', $categirue[0])->get();
-                }
 
+                foreach($filter['categories'] as $categirue){
+                    $categories_children = Categories::orWhere('parent_id', $categirue[0]);
+                }
+                $categories_children = $categories_children->get();
                 //function get_childrens($filter, $products){
 
-                $childrens = array();
-
                     foreach($categories_children as $category_children){
+                        $products = $products->orWhere('category_id', $category_children->id);
 
-                        array_push($childrens, $category_children->id);
+
                     }
-                    $products = $products->whereIn('category_id',$childrens);
 
                 //}
 
@@ -61,7 +62,7 @@ class HomeController extends Controller
                 $products = $products->where('price', '<=', $filter['price_max']);
             }
 
-
+            $products->paginate(get_paginate($request));
             return response()->json([
                 'data'=>$products->get(),
                 ]);
